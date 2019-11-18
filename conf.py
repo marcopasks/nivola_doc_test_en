7c2f75c31892a5f7c87e9102197116b3e8f4f8e0
2044 <marco.paschetta@csi.it>
2019-11-18 12:10:20 +0100

    aggiornato index2

--- conf.py
+++ conf.py
@@ -162,7 +162,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'yucca', 'Nivola Documentation',
+    (master_doc, 'nivola', 'Nivola Documentation',
      [author], 1)
 ]
 
