d97d5182cf17dc07497cb3b27aa7cd9c49731554
PASCHETTA Marco Andrea 2044 <marco.paschetta@csi.it>
2020-02-14 14:24:00 +0100

    Modifica css1

--- conf.py
+++ conf.py
@@ -108,8 +108,8 @@ html_favicon = 'favicon.ico'
 html_static_path = ['_static']
 def setup(app):
     # app.add_stylesheet("css/custom.css")
-    app.add_stylesheet("css/yucca.css")
-    app.add_javascript('js/yucca.js')
+    app.add_stylesheet("css/nivola.css")
+    app.add_javascript('js/nivola.js')
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
